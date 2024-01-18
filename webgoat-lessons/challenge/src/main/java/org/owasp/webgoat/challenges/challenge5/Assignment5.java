1ca20831b6e67dcb9f1f015c74ca863870e92307
Mobb autofixer <git@mobb.ai>
2024-01-18 12:08:26 +0000

    mobb fix commit: b9799ade-243d-48a3-b680-f42712e8336a

--- webgoat-lessons/challenge/src/main/java/org/owasp/webgoat/challenges/challenge5/Assignment5.java
+++ webgoat-lessons/challenge/src/main/java/org/owasp/webgoat/challenges/challenge5/Assignment5.java
@@ -57,7 +57,8 @@ public class Assignment5 extends AssignmentEndpoint {
             return failed(this).feedback("user.not.larry").feedbackArgs(username_login).build();
         }
         try (var connection = dataSource.getConnection()) {
-            PreparedStatement statement = connection.prepareStatement("select password from challenge_users where userid = '" + username_login + "' and password = '" + password_login + "'");
+            PreparedStatement statement = connection.prepareStatement("select password from challenge_users where userid = ? and password = '" + password_login + "'");
+            statement.setString(1, username_login);
             ResultSet resultSet = statement.executeQuery();
 
             if (resultSet.next()) {
